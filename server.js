9cbfe115a911d48b833b9abf445f495c371ad810
piszkos-somer <peratt816@hengersor.hu>
2025-01-22 11:17:02 +0100

    express

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3307,
+    password: "",
+    database: "kozutak",
+});
+
+app.listen(3001,  () => {
+    console.log("Server is running on port 3001");
+});
